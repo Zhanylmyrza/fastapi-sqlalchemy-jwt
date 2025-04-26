7c2d3f914230c6a301125febafcad571bf01d626
Zhanylmyrza <bkrvj83@gmail.com>
2025-04-26 11:34:16 +0600

    Authenticate Requests & Authorization

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Depends
+import models
+from database import engine
+from routers import auth, todos, admin, users
+
+app=FastAPI()
+
+models.Base.metadata.create_all(bind=engine) #will create a new database
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
+
+
+
